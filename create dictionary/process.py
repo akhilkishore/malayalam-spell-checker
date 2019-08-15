729e493d2d3fb5b58684d13db88330415eeee678
Akhil Kishore <akhilkishorek13@gmail.com.com>
2019-08-16 01:02:36 +0530

    updates

---
+++ create dictionary/process.py
@@ -0,0 +1,35 @@
+from gensim.models import FastText
+
+def getData():
+    files =["1.txt","2.txt"]
+    all_words =[]
+    for x in files:
+        f =  open(x,"r")
+        data = f.read()
+        data = data.split(" ")
+        items = []
+        numList = ["1","2","3","4","5","6","7","8","9","0"]
+        for y in data:
+            flag = 1
+            for z in numList:
+                if z in y:
+                    flag = 0
+                    break
+            if flag == 1:
+                if y not in items and y != "" and y !=" ":
+                    y = y.replace(".","")
+                    y =  y.replace(" ","")
+                    if len(y) >2:
+                        items.append(y)
+        for y in items:
+            if y not in all_words:
+                all_words.append(y)
+    print(len(all_words))
+    return all_words
+
+def main():
+    words = getData()
+    model_ted = FastText(words, size=100, window=5, min_count=5, workers=4,sg=1)
+main()
+
+
--- create dictionary/wiki scraper/scraperTwo.py
+++ create dictionary/wiki scraper/scraperTwo.py
@@ -6,22 +6,56 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from parsel import Selector 
 
-f = open("hrefFList.txt", "r")
 
-data = f.read()
+#data = f.read()
 
-data = data.split("*")
+#data = data.split("*")
 
+data =["https://ml.wikipedia.org/wiki/%E0%B4%85%E0%B4%AA%E0%B5%8D%E0%B4%AA%E0%B5%81_%E0%B4%A8%E0%B5%86%E0%B4%9F%E0%B5%81%E0%B4%99%E0%B5%8D%E0%B4%99%E0%B4%BE%E0%B4%9F%E0%B4%BF"]
 def getWebpage_soup(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     soup =  BeautifulSoup(webpage, 'html.parser')
     return soup
-
+links = []
+all = []
 for x in data:
-    if x != "/":
+    if x != "":
+        #print(x)
+        try:
+                soup = getWebpage_soup(x)
+                #text = soup.find_all(text=True)
+                #print(text)
+                #break
+                data = []
+                for p in soup.find_all('p'):
+                        #print(p.text)
+                        data.append(p.text)
+                all.append(data)
+                #link = soup.find_all('a', href=)
+                for a in soup.find_all('a', href=True):
+                        if "/wiki/%" in a['href']:
+                                #print(a['href'])
+                                #print("\n\n")
+                                if "https://ml.wikipedia.org"+a['href'] not in links:
+                                        links.append("https://ml.wikipedia.org"+a['href'])
+        except:
+                pass
+
+f = open("demofile2.txt", "a")
+for x in links:
+    if x != "":
         print(x)
-        soup = getWebpage_soup("https://www.mathrubhumi.com"+x)
-        text = soup.find_all(text=True)
-        print(text)
-        break
+        #print(x)
+        soup = getWebpage_soup(x)
+        #text = soup.find_all(text=True)
+        f.write("Now the file has more content!")
+        f.close()
+        #print(text)
+        #break
+        data = []
+        for p in soup.find_all('p'):
+                #print(p.text)
+                data.append(p.text)
+        all.append(data)
+print(links)
